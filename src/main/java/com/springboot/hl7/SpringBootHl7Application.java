1a16abd493a19c9d374effe2c5527850d74ebaa7
KARUPPASAMY MARIAPPAN <karuppasamyit10@gmail.com>
2017-03-30 20:18:04 +0530

    Initially Setup the Spring boot application

---
+++ src/main/java/com/springboot/hl7/SpringBootHl7Application.java
@@ -0,0 +1,12 @@
+package com.springboot.hl7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHl7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHl7Application.class, args);
+	}
+}
---
+++ src/test/java/com/springboot/hl7/SpringBootHl7ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.hl7;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHl7ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
